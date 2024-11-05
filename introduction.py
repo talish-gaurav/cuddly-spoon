3d03e69a30dab41713992756aaa8b2e5f4f1b43b
talish-gaurav <97028189+talish-gaurav@users.noreply.github.com>
2024-11-05 18:58:02 +0000

    Add files via upload

---
+++ introduction.py
@@ -0,0 +1,36 @@
+a=15
+b=9
+c=18
+
+if a==b and a==c:
+
+     print(" all values are equal")
+
+else:
+     
+     print(" at least one value is equal")
+
+if a==b or a==c:
+
+     print(" one of the values is equal")
+
+else:
+     
+     print(" none of the values are equal")  
+
+
+if a!=b and a!=c:
+
+     print(" all values are not equal")
+
+else:
+     
+     print(" at least one value is not equal")
+
+if a!=b or a!=c:
+
+     print(" one of the values is not equal")
+
+else:
+     
+     print(" all of the values are not equal")    
